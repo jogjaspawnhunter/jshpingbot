1004413d4e499d9b09b096afdade90a599d2c8cc
jogjaspawnhunter <71266376+jogjaspawnhunter@users.noreply.github.com>
2020-09-14 19:54:32 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'config',
     description: "this is a config login discord!",
     execute(message, args){
-        message.channel.send('NzUwMjc3ODgzMzU1ODU2OTY3.X04Mug.9kg694P6TQfv0tP5lLud98H1TN8');
+        message.channel.send('BOT_TOKEN_HERE');
     }
 }
