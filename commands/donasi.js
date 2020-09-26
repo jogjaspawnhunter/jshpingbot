ad0e1e22a82e02b586ee2ad1ed14c2b9d0611c28
jogjaspawnhunter <71266376+jogjaspawnhunter@users.noreply.github.com>
2020-09-27 02:54:12 +0700

    Update donasi.js

--- commands/donasi.js
+++ commands/donasi.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'donasi',
-    description: "this is a donasi command!",
+    description: "Cara untuk melakukan Donasi",
     execute(message, args){
         message.channel.send('Untuk Donasi sementara cek pengumuman yang ada di <#742664544706625546> dan <#742411433836413032>\nDan para Admin dan Moderator akan memandumu dalam melakukan Donasi\nTerima Kasih');
     }
