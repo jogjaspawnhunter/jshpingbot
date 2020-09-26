bf78e0947b75415e5892f0b169de027788f49feb
jogjaspawnhunter <71266376+jogjaspawnhunter@users.noreply.github.com>
2020-09-27 02:54:49 +0700

    Update error.js

--- commands/error.js
+++ commands/error.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'error',
-    description: "this is a error command!",
+    description: "Cara menanggulangi apabila terjadi ERROR pada Map/Scanner/Device kamu ada di sini",
     execute(message, args){
         message.channel.send('Apabila kamu mengalami ERROR apapun dengan scanner kamu\nBaik itu Device HP atau ATV kamu\nCobalah untuk RESTART Device kamu dan tunggu sekitar 10 menit\nApabila masih terjadi ERROR silakan tanya di Channel <#750301599439781948>');
     }
