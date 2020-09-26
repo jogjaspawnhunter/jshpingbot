7fd52d1cb45e1df375308f47663f0287a9002113
jogjaspawnhunter <71266376+jogjaspawnhunter@users.noreply.github.com>
2020-09-27 02:56:59 +0700

    Update quest.js

--- commands/quest.js
+++ commands/quest.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'quest',
-    description: "this is a quest command!",
+    description: "Info update & Pengetahuan umum tentang quest pada Map kita",
     execute(message, args){
         message.channel.send('Quest Scanning akan otomatis reset pada pukul 0:00 WIB,\nApabila ada ERROR atau RESET Bulanan atau Event baru,\nMohon infokan kepada Admin & Moderator ya Gaes, Thanks');
     }
