d71d55601bd625a5abca6905d757d961a0f4a8e7
jogjaspawnhunter <71266376+jogjaspawnhunter@users.noreply.github.com>
2020-09-27 02:56:11 +0700

    Update mega.js

--- commands/mega.js
+++ commands/mega.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'mega',
-    description: "this is a mega command!",
+    description: "Info tentang Mega update/feature",
     execute(message, args){
         message.channel.send('Versi Map saat ini Sudah Support Mega Energy Quest secara penuh,\nApabila ditemukan ERROR saat Scan `undefined` pada Reward, laporkan pada Admin & Momod,\nDan jangan lupa, Mohon bersabar ya, Thanks');
     }
