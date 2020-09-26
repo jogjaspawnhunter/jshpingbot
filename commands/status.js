25ded9355c4640f51a9892ba3320ee445e354d38
jogjaspawnhunter <71266376+jogjaspawnhunter@users.noreply.github.com>
2020-09-27 02:57:33 +0700

    Update status.js

--- commands/status.js
+++ commands/status.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'status',
-    description: "this is a status command!",
+    description: "Info Status Versi Map dan Device yang support untuk dijalankan",
     execute(message, args){
         message.channel.send('**Status Devices** : `4 pcs LIVE` - `0 pcs DOWN`\nStatus Devices apabila ada DOWN, tunggu **Restart & Update** dari yang pegang Devices ya\n\n**Versi Map sekarang sudah support PoGo versi** : `0.187.1`\n\n**PogoDroid bisa di download di**: `http://maddev.eu/apk/PogoDroid.apk`\n\n**RGC bisa di download di**: `https://github.com/Map-A-Droid/MAD/blob/master/APK/RemoteGpsController.apk`');
     }
