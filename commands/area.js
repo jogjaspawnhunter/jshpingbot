88d190c50f49b5c33551e36a7217d529d61bc03c
jogjaspawnhunter <71266376+jogjaspawnhunter@users.noreply.github.com>
2020-09-27 02:53:09 +0700

    Update area.js

--- commands/area.js
+++ commands/area.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'area',
-    description: "this is a area command!",
+    description: "Cara membuat file Area/Geofence",
     execute(message, args){
         message.channel.send('**Buat Geofence/Area scan kamu di:** https://www.gpsvisualizer.com/draw/\nKemudian **Download** file txt/gpx dari Area/Geofence yang telah kalian buat\nLalu beritahu admin/mod di channel <#750301599439781948>\n\nTerima Kasih');
     }
