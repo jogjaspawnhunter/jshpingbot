224087f0d225af2d6c7958df37ce1267c743cd49
jogjaspawnhunter <71266376+jogjaspawnhunter@users.noreply.github.com>
2020-09-27 02:53:35 +0700

    Update bayar.js

--- commands/bayar.js
+++ commands/bayar.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'bayar',
-    description: "this is a bayar command!",
+    description: "Cara untuk melakukan Kontribusi/Patungan bulanan untuk Map kita tercinta",
     execute(message, args){
         message.channel.send('Untuk pembayaran sementara cek pengumuman yang ada di <#742664544706625546> dan <#742411433836413032>\nDan para Admin dan Moderator akan memandumu dalam pembayaran\nTerima Kasih');
     }
