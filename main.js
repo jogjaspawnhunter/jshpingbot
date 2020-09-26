2d10c7366899a94dfd241237d88d562c9a0426ec
jogjaspawnhunter <71266376+jogjaspawnhunter@users.noreply.github.com>
2020-09-27 03:11:09 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,37 +1,74 @@
-const Discord = require('discord.js');
-const config = require('./config/config.json');
-const client = new Discord.Client();
-const prefix = '!';
 const fs = require('fs');
+const Discord = require('discord.js');
+const { prefix, token } = require('./config/config.json');
 
+const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for(const file of commandFiles){
-    const command = require(`./commands/${file}`);
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-    client.commands.set(command.name, command);
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
 }
 
+const cooldowns = new Discord.Collection();
+
 client.once('ready', () => {
-    console.log('Willow is online!')
+	console.log('Ready!');
 });
 
-client.on('message', message =>{
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const commandName = args.shift().toLowerCase();
 
-    if (!client.commands.has(command)) return;
+	const command = client.commands.get(commandName)
+		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
-    try {
-	    client.commands.get(command).execute(message, args);
-    } catch (error) {
-	    console.error(error);
-	    message.reply('ku tak tahu apa maksudmu, coba kirim `!help`');
-    }
-})
+	if (!command) return;
 
+	if (command.guildOnly && message.channel.type === 'dm') {
+		return message.reply('Saya tidak bisa menggunakan perintah itu dalam DM');
+	}
 
-client.login(config.token);
+	if (command.args && !args.length) {
+		let reply = `Kamu tidak menyertakan arguments, ${message.author}!`;
+
+		if (command.usage) {
+			reply += `\nCara menggunakan arguments: \`${prefix}${command.name} ${command.usage}\``;
+		}
+
+		return message.channel.send(reply);
+	}
+
+	if (!cooldowns.has(command.name)) {
+		cooldowns.set(command.name, new Discord.Collection());
+	}
+
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.name);
+	const cooldownAmount = (command.cooldown || 3) * 1000;
+
+	if (timestamps.has(message.author.id)) {
+		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+		if (now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+		}
+	}
+
+	timestamps.set(message.author.id, now);
+	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+	try {
+		command.execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('Ada error dalam melakukan perintah itu');
+	}
+});
+
+client.login(token);
