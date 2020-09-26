e5197c983b7c82e53c04300721d0743968aad4ee
jogjaspawnhunter <71266376+jogjaspawnhunter@users.noreply.github.com>
2020-09-27 02:58:56 +0700

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'update',
-    description: "this is a update command!",
+    description: "Cara update map/scanner/device kamu",
     execute(message, args){
         message.channel.send('Cara update scanner anda bisa tanya di Channel <#750301599439781948>\nDan segera berdoa sebanyak-banyaknya.');
     }
