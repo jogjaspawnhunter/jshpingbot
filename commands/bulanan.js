916bba56ea656c3af48641fee66f6d83869bf487
jogjaspawnhunter <71266376+jogjaspawnhunter@users.noreply.github.com>
2020-10-02 15:40:09 +0700

    Create bulanan.js

---
+++ commands/bulanan.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'bulanan',
+    description: "Info tentang waktu dan laporan Bulanan iuran/donasi",
+    execute(message, args){
+        message.channel.send('Ini laporan keuangan web mapping kita ada di link ini:\nhttps://tinyurl.com/yyzmnnmr \nJatuh tempo per tanggal 9 setiap bulannya ya Gaes\nTerima Kasih');
+    }
+}
