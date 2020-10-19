612144e55370742459478afcef619afd10f8ee78
jogjaspawnhunter <71266376+jogjaspawnhunter@users.noreply.github.com>
2020-10-19 17:32:36 +0700

    Update status.js

--- commands/status.js
+++ commands/status.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'status',
     description: "Info Status Versi Map dan Device yang support untuk dijalankan",
     execute(message, args){
-        message.channel.send('**Status Devices** : `3 pcs LIVE` - `1 pcs DOWN` - `1 pcs MANUAL RESPOND NEEDED`\nStatus Devices apabila ada DOWN, tunggu **Restart & Update** dari yang pegang Devices ya\nStatus Devices apabila NEED MANUAL RESPOND, tunggu **Fix & Restart** dari pemegang device\n\n**Versi Map sekarang sudah support PoGo versi** : `0.187.2` `0.189.0`\n\n**PogoDroid bisa di download di**: `http://maddev.eu/apk/PogoDroid.apk`\n\n**RGC bisa di download di**: `https://github.com/Map-A-Droid/MAD/blob/master/APK/RemoteGpsController.apk`');
+        message.channel.send('**Status Devices** : `3 pcs LIVE` - `1 pcs DOWN` - `1 pcs MANUAL RESPOND NEEDED`\nStatus Devices apabila ada DOWN, tunggu **Restart & Update** dari yang pegang Devices ya\nStatus Devices apabila NEED MANUAL RESPOND, tunggu **Fix & Restart** dari pemegang device\n\n**Versi Map sekarang sudah support PoGo versi** : `0.187.2` `0.189.0`\n**PogoDroid bisa di download di**: `http://maddev.eu/apk/PogoDroid.apk`\n**RGC bisa di download di**: `https://github.com/Map-A-Droid/MAD/blob/master/APK/RemoteGpsController.apk`');
     }
 }
