01658c701bbc9c8ba4276215f89196b1134a785e
jogjaspawnhunter <71266376+jogjaspawnhunter@users.noreply.github.com>
2020-11-21 02:23:42 +0700

    Create pembukuan.js

---
+++ commands/pembukuan.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'pembukuan',
+    description: "Info tentang Pembukuan di Google Sheet untuk Membership/Subscription Web Map kita ini.",
+    execute(message, args){
+        message.channel.send('**Link Url Pembukuan** : `https://tinyurl.com/yyzmnnmr`\n**Bila ada pertanyaan, silakan kontak admin/mod**');
+    }
+}
