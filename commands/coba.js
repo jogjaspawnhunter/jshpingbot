76830a29f46beeeb7253be3a941a356282b676b0
jogjaspawnhunter <71266376+jogjaspawnhunter@users.noreply.github.com>
2020-12-14 22:39:18 +0700

    Delete coba.js

--- commands/coba.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'coba',
-    description: "cobasaja",
-    execute(message, args){
-        message.channel.send('**Status Devices :**\n<788064501131509810>\n<788065325844660274>\n<788065440500023317>\nThanks.');
-    }
-}
