fa9a8d7faf16ad7c7c811ad1c5bf77143030ccf0
jogjaspawnhunter <71266376+jogjaspawnhunter@users.noreply.github.com>
2020-12-14 22:34:54 +0700

    Update coba.js

--- commands/coba.js
+++ commands/coba.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'coba',
-    description: "Cara update map/scanner/device kamu",
+    description: "cobasaja",
     execute(message, args){
-        message.channel.send('Status <#788061193050521651>\nChannel ini `<#788061193050521651>`\nThanks.');
+        message.channel.send('**Status Devices :**\n<788064501131509810>\n<788065325844660274>\n<788065440500023317>\nThanks.');
     }
 }
