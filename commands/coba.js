db462e8cc0742c5258db2e265f6ca1af1594d9a0
jogjaspawnhunter <71266376+jogjaspawnhunter@users.noreply.github.com>
2020-12-14 22:18:52 +0700

    Update coba.js

--- commands/coba.js
+++ commands/coba.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'coba',
 	description: 'Info tentang group discord kita ini',
 	execute(message) {
-		message.channel.send(`Status: <#788061193050521651>`\nJumlah total member: ${message.guild.memberCount}`);
+		message.channel.send('`Status: <#788061193050521651>`\nJumlah total member: ${message.guild.memberCount}');
 	},
 };
