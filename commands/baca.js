2bc2336c9539520e6814b30734dedda7b7a42838
jogjaspawnhunter <71266376+jogjaspawnhunter@users.noreply.github.com>
2020-12-23 23:28:50 +0700

    Create baca.js

---
+++ commands/baca.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'baca',
+    description: "Buat yang gak BACA SAMA SEKALI",
+    execute(message, args){
+        message.channel.send('Membaca adalah pencerdasan Bangsa');
+    }
+}
