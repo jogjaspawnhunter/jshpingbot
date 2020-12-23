ffac1441baf0a1e864d17efe9a95533897992497
jogjaspawnhunter <71266376+jogjaspawnhunter@users.noreply.github.com>
2020-12-23 23:35:26 +0700

    Update baca.js

--- commands/baca.js
+++ commands/baca.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'baca',
     description: "Buat yang gak BACA SAMA SEKALI",
     execute(message, args){
-        message.channel.send('Membaca adalah pencerdasan Bangsa');
+        message.channel.send('Membaca adalah pencerdasan Bangsa\nCek <#750301599439781948>, <#746765672905965639>, <#742664544706625546>');
     }
 }
