4677d779c690af5f024f7cb5db012ef4765a036b
jogjaspawnhunter <71266376+jogjaspawnhunter@users.noreply.github.com>
2020-12-24 00:21:44 +0700

    Delete test.js

--- commands/test.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'test',
-    description: "Info Status Versi Map dan Device yang support untuk dijalankan",
-    execute(message, args){
-        const chan1 = channel.messages.cache.get("788061193050521651");
-        message.channel.send('**Status Devices** : ${chan1}\n<#788065325844660274>\nCek info lebih lanjut di channel : <#746765672905965639>\n\n**Versi Map sekarang sudah support PoGo versi** : <#788067062840164362>\n**PogoDroid bisa di download di**: `http://maddev.eu/apk/PogoDroid.apk`\n**RGC bisa di download di**: `https://github.com/Map-A-Droid/MAD/blob/master/APK/RemoteGpsController.apk`');
-    }
-}
