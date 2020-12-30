18be028b79ab132ae2c83981b97fa0de1a55a41b
jogjaspawnhunter <71266376+jogjaspawnhunter@users.noreply.github.com>
2020-12-31 00:39:14 +0700

    Update and rename status.js to check.js

--- commands/check.js
+++ commands/check.js
@@ -1,6 +1,6 @@
 module.exports = {
-    name: 'status',
-    description: "Info Status Versi Map dan Device yang support untuk dijalankan",
+    name: 'check',
+    description: "Check Status Versi Map dan Device yang support untuk dijalankan",
     execute(message, args){
         message.channel.send('**Status Devices** : <#788064501131509810>\n<#788065325844660274>\nCek info lebih lanjut di channel : <#746765672905965639>\n\n**Versi PoGo untuk Map** : <#788067062840164362>\n**PogoDroid versi**: <#793061450775265290>\n**RGC versi**: <#793061832884617256>');
     }
