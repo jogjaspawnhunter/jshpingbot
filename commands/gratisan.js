753e0f82d07355c7d56ec007fd09a84c7a90d74a
jogjaspawnhunter <71266376+jogjaspawnhunter@users.noreply.github.com>
2021-01-12 17:33:38 +0700

    Update gratisan.js

--- commands/gratisan.js
+++ commands/gratisan.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'gratisan',
     description: "Apabila ada giveaway atau yang gratisan ada di sini",
     execute(message, args){
-        message.channel.send('Mau kencing aja bayar, dan Kopi pun semakin Mahal.\nBila tak berkenan berkontribusi bersama,\nSilakan bikin sendiri, gampang koq :smile:');        
+        message.channel.send('Sekarang sedang ada Giveaway Access Web Map kita,\nSelama periode 11 Januari 2021 - 25 Januari 2021 \nDM atau PM ke <328233262558216193> dan beritahu kontak anda yang ada di Group WA JRH\n**Syarat : Member Aktif di group WA Jogja Raid Hunter**');        
     //    message.channel.send('Mau kencing aja bayar, dan Kopi pun semakin Mahal.\nBila tak berkenan berkontribusi bersama,\nSilakan bikin sendiri, gampang koq :smile:');
     }
 }
