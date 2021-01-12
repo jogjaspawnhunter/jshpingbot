561d9daf12c6504905aed8e9c2f28cdf5b858f02
jogjaspawnhunter <71266376+jogjaspawnhunter@users.noreply.github.com>
2021-01-12 16:51:01 +0700

    Update gratisan.js

--- commands/gratisan.js
+++ commands/gratisan.js
@@ -2,6 +2,7 @@ module.exports = {
     name: 'gratisan',
     description: "Apabila ada giveaway atau yang gratisan ada di sini",
     execute(message, args){
-        message.channel.send('Mau kencing aja bayar, dan Kopi pun semakin Mahal.\nBila tak berkenan berkontribusi bersama,\nSilakan bikin sendiri, gampang koq :smile:');
+        message.channel.send('Mau kencing aja bayar, dan Kopi pun semakin Mahal.\nBila tak berkenan berkontribusi bersama,\nSilakan bikin sendiri, gampang koq :smile:');        
+    //    message.channel.send('Mau kencing aja bayar, dan Kopi pun semakin Mahal.\nBila tak berkenan berkontribusi bersama,\nSilakan bikin sendiri, gampang koq :smile:');
     }
 }
